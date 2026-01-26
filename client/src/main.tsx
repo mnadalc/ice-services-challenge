85ac16fe45e3ec1b45cdec529ddde3de5fa4efd1
Miguel Nadal <11161738+mnadalc@users.noreply.github.com>
2026-01-26 15:51:35 +0100

    feat(client): Add SongsTable component with mocked data

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import { SongsTable } from './components/SongsTable.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <main className="container mx-auto px-4 py-8">
+      <SongsTable />
+    </main>
   </StrictMode>,
 );
